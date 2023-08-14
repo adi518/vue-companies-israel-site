91e88252b401fc47cb4afbf7c3dc4abb90bfe5ec
adi518 <adisahar1@hotmail.com>
2023-08-15 02:44:36 +0300

    Update index.js

--- packages/vue-companies-israel-site/src/flatten-routes/index.js
+++ packages/vue-companies-israel-site/src/flatten-routes/index.js
@@ -1,27 +1,32 @@
 export function flattenRoutes(routes, level = 0, parent = {}) {
-  if (Array.isArray(routes)) {
-    return routes.reduce((flatRoutes, route) => {
-      const { name, path, children, ...restRoute } = route;
-      const isRoot = level === 0;
-      const nested = !isRoot;
-      flatRoutes.push({
-        ...restRoute,
-        name,
-        path,
-        level,
-        isRoot,
-        nested,
-        parent: {
-          ...parent,
-          name: isRoot ? undefined : parent.name,
-        },
-      });
-      if (Array.isArray(children) && children.length) {
-        flatRoutes = flatRoutes.concat(
-          flattenRoutes(children, level + 1, route)
-        );
-      }
-      return flatRoutes;
-    }, []);
+  if (!Array.isArray(routes)) {
+    return
   }
+
+  return routes.reduce((flatRoutes, route) => {
+  const { name, path, children, ...restRoute } = route;
+  const isRoot = level === 0;
+  const nested = !isRoot;
+
+  flatRoutes.push({
+    ...restRoute,
+    name,
+    path,
+    level,
+    isRoot,
+    nested,
+    parent: {
+      ...parent,
+      name: isRoot ? undefined : parent.name,
+    },
+  });
+
+  if (Array.isArray(children) && children.length) {
+    flatRoutes = flatRoutes.concat(
+      flattenRoutes(children, level + 1, route)
+    );
+  }
+
+  return flatRoutes;
+}, []);
 }
