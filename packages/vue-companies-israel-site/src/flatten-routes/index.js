ae08e49f40f3dd9ce835fac8500526efae1fbdb1
adi518 <adisahar1@hotmail.com>
2023-08-15 02:45:55 +0300

    Update index.js

--- packages/vue-companies-israel-site/src/flatten-routes/index.js
+++ packages/vue-companies-israel-site/src/flatten-routes/index.js
@@ -4,29 +4,29 @@ export function flattenRoutes(routes, level = 0, parent = {}) {
   }
 
   return routes.reduce((flatRoutes, route) => {
-  const { name, path, children, ...restRoute } = route;
-  const isRoot = level === 0;
-  const nested = !isRoot;
-
-  flatRoutes.push({
-    ...restRoute,
-    name,
-    path,
-    level,
-    isRoot,
-    nested,
-    parent: {
-      ...parent,
-      name: isRoot ? undefined : parent.name,
-    },
-  });
-
-  if (Array.isArray(children) && children.length) {
-    flatRoutes = flatRoutes.concat(
-      flattenRoutes(children, level + 1, route)
-    );
-  }
-
-  return flatRoutes;
+    const { name, path, children, ...restRoute } = route;
+    const isRoot = level === 0;
+    const nested = !isRoot;
+  
+    flatRoutes.push({
+      ...restRoute,
+      name,
+      path,
+      level,
+      isRoot,
+      nested,
+      parent: {
+        ...parent,
+        name: isRoot ? undefined : parent.name,
+      },
+    });
+  
+    if (Array.isArray(children) && children.length) {
+      flatRoutes = flatRoutes.concat(
+        flattenRoutes(children, level + 1, route)
+      );
+    }
+  
+    return flatRoutes;
 }, []);
 }
